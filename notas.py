d8f40d21e1f43ae94896d15c0c0413218c582aa5
FCS-dev <107736390+FCS-dev@users.noreply.github.com>
2025-10-28 20:00:24 +0100

    Practica de git y github 20251028

---
+++ notas.py
@@ -0,0 +1,18 @@
+def promedio(nota1, nota2, nota3):
+    return (nota1 + nota2 + nota3) / 3
+
+
+notas = [
+    {"alumno": "Fernandito", "nota1": 8, "nota2": 7, "nota3": 6.5},
+    {"alumno": "Josefina", "nota1": 8, "nota2": 8.5, "nota3": 5.5},
+    {"alumno": "Arturito", "nota1": 5, "nota2": 4.5, "nota3": 5},
+]
+
+for alumno in notas:
+    print(f"Alumno  :{alumno['alumno']}")
+    print(f"Nota 1  :{alumno['nota1']}")
+    print(f"Nota 2  :{alumno['nota2']}")
+    print(f"Nota 3  :{alumno['nota3']}")
+    media = promedio(alumno["nota1"], alumno["nota2"], alumno["nota3"])
+    print(f"Promedio:{media:.2f}")
+    print("aprobado" if media >= 5 else "suspendido")
